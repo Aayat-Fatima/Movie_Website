45f9c47ce7e6cf2b8a1e0b6e54473286431de542
Mohammad Sarfraz <mdsarfraz4499@gmail.com>
2024-11-22 19:13:33 +0530

    making movie website

---
+++ script.js
@@ -0,0 +1,9 @@
+// Handle active tab switching
+const tabs = document.querySelectorAll('.tab');
+
+tabs.forEach(tab => {
+  tab.addEventListener('click', () => {
+    document.querySelector('.tab.active').classList.remove('active');
+    tab.classList.add('active');
+  });
+});
